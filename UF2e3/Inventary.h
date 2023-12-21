289b3ad39ed4888003a9e723ec96629151f8b6b9
Marc Ruiz Garcia <marc.ruiz.garcia@enti.cat>
2023-12-21 11:15:34 +0100

    punteros

--- UF2e3/Inventary.h
+++ UF2e3/Inventary.h
@@ -5,6 +5,13 @@ struct Inventary{
 	int slots;
 	vector<Item> items;
 
+	Item* armor;
+	Item* helmet;
+	Item* glover;
+	Item* boots;
+	Item* weapon;
+	Item* shield;
+
 	void Initialize();
 	string PrintItems();
 	bool AddItem(Item newItem);
